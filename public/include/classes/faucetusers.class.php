ba3321e108bafbe2348cdbb9418cba41e6b99fad
Zen00 <zenenterprise@gmail.com>
2014-02-04 11:21:11 -0700

    querying user instead

--- public/include/classes/faucetusers.class.php
+++ public/include/classes/faucetusers.class.php
@@ -11,9 +11,9 @@ class Faucetusers extends Base {
    * Log the information from a user faucet request
    **/
   public function logUser() {
-    $userIP = getCurrentIP();
+    $userIP = $this->user->getCurrentIP();
     $userAddress = $_POST['userAddress'];
-    $stmt = $this->$mysqli->prepare("INSERT INTO $this->table (user_address, user_ip) VALUES (?,?)");
+    $stmt = $this->mysqli->prepare("INSERT INTO $this->table (user_address, user_ip) VALUES (?,?)");
     $stmt->bind_param('ss',$userAddress,$userIP);
     $stmt->execute();
 }
@@ -33,38 +33,6 @@ class Faucetusers extends Base {
     return $this->getSingle($userIP, 'user_IP', 'id');
 }
   
-  /**
-   * Convenience function to get IP address, no params is the same as REMOTE_ADDR
-   * @param trustremote bool must be FALSE to checkclient or checkforwarded
-   * @param checkclient bool check HTTP_CLIENT_IP for a valid ip first
-   * @param checkforwarded bool check HTTP_X_FORWARDED_FOR for a valid ip first
-   * @return string IP address
-   */
-  public function getCurrentIP($trustremote=true, $checkclient=false, $checkforwarded=false) {
-    $client = (isset($_SERVER['HTTP_CLIENT_IP'])) ? $_SERVER['HTTP_CLIENT_IP'] : false;
-    $fwd = (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : false;
-    $remote = (isset($_SERVER['REMOTE_ADDR'])) ? $_SERVER['REMOTE_ADDR'] : @$_SERVER['REMOTE_ADDR'];
-    // shared internet
-    if (filter_var($client, FILTER_VALIDATE_IP) && !$trustremote && $checkclient) {
-      return $client;
-    } else if (strpos($fwd, ',') !== false && !$trustremote && $checkforwarded) {
-      // multiple proxies
-      $ips = explode(',', $fwd);
-      $path = array();
-      foreach ($ips as $ip) {
-        if (filter_var($ip, FILTER_VALIDATE_IP)) {
-          $path[] = $ip;
-        }
-      }
-      return array_pop($path);
-    } else if (filter_var($fwd, FILTER_VALIDATE_IP) && !$trustremote && $checkforwarded) {
-      // single
-      return $fwd;
-    } else {
-      // as usual
-      return $remote;
-    }
-  }
 }
 
 // Make our class available automatically
