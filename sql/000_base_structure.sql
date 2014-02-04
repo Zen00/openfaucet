46b276402bb4559212e8d44d472128ba4f1b91ba
Zen00 <zenenterprise@gmail.com>
2014-02-04 08:35:39 -0700

    Reverted change

--- sql/000_base_structure.sql
+++ sql/000_base_structure.sql
@@ -140,7 +140,7 @@ CREATE TABLE IF NOT EXISTS `settings` (
   UNIQUE KEY `setting` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `settings` (`name`, `value`) VALUES ('DB_VERSION', '0.0.4') ON DUPLICATE KEY UPDATE;
+INSERT INTO `settings` (`name`, `value`) VALUES ('DB_VERSION', '0.0.4');
 
 CREATE TABLE IF NOT EXISTS `shares` (
   `id` bigint(30) NOT NULL AUTO_INCREMENT,
