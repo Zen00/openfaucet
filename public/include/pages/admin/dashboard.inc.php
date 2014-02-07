9526a76a21d18708e7e331a674f4736ffb409a78
Zen00 <zenenterprise@gmail.com>
2014-02-07 06:32:29 -0700

    Found typo

--- public/include/pages/admin/dashboard.inc.php
+++ public/include/pages/admin/dashboard.inc.php
@@ -13,7 +13,7 @@ if ($bitcoin->can_connect() === true){
   $aGetInfo = $bitcoin->getinfo();
 } else {
   $aGetInfo = array('errors' => 'Unable to connect');
-  $_SESSION['POPUP'][] = array('CONTENT' => 'Unable to connect to wallet RPC service: ' . $bitcoin>can_connect(), 'TYPE' => 'errormsg');
+  $_SESSION['POPUP'][] = array('CONTENT' => 'Unable to connect to wallet RPC service: ' . $bitcoin->can_connect(), 'TYPE' => 'errormsg');
 }
 
 // Fetch version information
