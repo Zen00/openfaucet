d735ea55c57f2d76e7eee0ddc8788018cdf49d72
Zen00 <zenenterprise@gmail.com>
2014-02-07 04:20:08 -0700

    This should work from prior experience...

--- public/include/classes/bitcoin.class.php
+++ public/include/classes/bitcoin.class.php
@@ -292,17 +292,11 @@ class BitcoinClient extends jsonRPCClient {
    */
   public function can_connect() {
     try {
-      $r = getinfo();
+      $r = parent::getinfo();
     } catch (Exception $e) {
       return $e->getMessage();
     }
     return true;
-  }
-  
-  public function getinfo() {
-    $this->oDebug->append("STA " . __METHOD__, 4);
-    if ($data = $this->memcache->get(__FUNCTION__)) return $data;
-    return $this->memcache->setCache(__FUNCTION__, parent::getinfo(), 30);
   }
 
     public function validateaddress($coin_address) {
